d392fc3256260a93b31298c263aa604ba2f6af28
andre <andreina.vargas@gmail.com>
2018-07-02 08:33:15 +0200

    creating an basic html

---
+++ main0.js
@@ -0,0 +1,43 @@
+'use strict';
+
+var adalabers = [
+  {
+    name: 'Maria',
+    job: 'diseñadora',
+    age: 29,
+  },
+  {
+    name: 'lucia',
+    job: 'ingeniera química',
+    age: 31,
+  },
+  {
+    name: 'Susana',
+    job: 'periodista',
+    age: 34,
+  },
+  {
+    name: 'Susana',
+    job: 'periodista',
+    age: 34,
+  },
+  {
+    name: 'Rocio',
+    job: 'diseñadora',
+    age: 25,
+  },
+  {
+    name: 'Inmaculada',
+    job: 'chef',
+    age: 37,
+  },
+
+];
+
+function countAdalabers(){
+  var numerodeninas= adalabers.length;
+  alert(numerodeninas);
+
+}
+
+countAdalabers();
